7da3c9c2cdff510064775571ba31aa2901b20ef9
Calterat <Calterat@gmail.com>
2020-12-11 00:49:12 -0600

    finished Engineer tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,5 +2,18 @@ const Engineer = require('../lib/Engineer');
 
 test("Create an Engineer", () => {
     const engineer = new Engineer('sarah', 123, 'sarah@test.com', 'sarahGit');
-    
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
 })
+
+test("Create an Engineer", () => {
+    const engineer = new Engineer('sarah', 123, 'sarah@test.com', 'sarahGit');
+    expect(engineer.github).toBe('sarahGit');
+});
+
+test("Create an Engineer", () => {
+    const engineer = new Engineer('sarah', 123, 'sarah@test.com', 'sarahGit');
+    expect(engineer.getRole()).toBe('Engineer');
+});
