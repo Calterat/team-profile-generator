87b2f7a58f39ff4a314bbf72580547009e8aef5c
Calterat <Calterat@gmail.com>
2020-12-12 17:57:32 -0600

    finished turning data into prototypes

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const { prompt } = require('./lib/Prompt');
+const { toClasses } = require('./lib/Classes');
 const writeHTML = require('./lib/HTML');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 prompt()
     .then(team => 
+        toClasses(team))
+    .then(team => 
         writeHTML(team))
-    .then(HTML => console.log(HTML));
+    .catch(err => console.log(err));
+    // .then(team => 
+    //     console.log(team));
+    // .then(team => 
+    //     writeHTML(team))
+    // .then(HTML => console.log(HTML));
