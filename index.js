808b0d37f0c99df2ed540f8b1ef021beba697cee
Calterat <Calterat@gmail.com>
2020-12-12 15:37:03 -0600

    started the HTML generation

--- index.js
+++ index.js
@@ -1,69 +1,7 @@
-const inquirer = require('inquirer');
-const writeHTML = require('./lib/page-template');
+const { prompt } = require('./lib/Prompt');
+const writeHTML = require('./lib/HTML');
 
-const team = [];
-
-const prompt = _ => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message: 'What role would you like to add to your team?',
-            choices: ['Manager', 'Engineer', 'Intern', 'Employee']
-        },
-        {
-            type: "input",
-            name: "name",
-            message: "Input Name Here: "
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Input ID Here: '
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Input Email Here: '
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'Input Office Number Here: ',
-            when: ({ role }) => {
-                if (role === 'Manager') return true;
-                return false;
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Input Github Username Here: ',
-            when: ({ role }) => {
-                if (role === 'Engineer') return true;
-                return false;
-            }
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: 'Input School Here: ',
-            when: ({ role }) => {
-                if (role  === 'Intern') return true;
-                return false;
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'newMember',
-            message: 'Would you like to add another person to the team?',
-            default: false
-        }
-    ])
-    .then(teamMember => {
-        team.push(teamMember);
-        if (teamMember.newMember) prompt();
-    })
-}
-
-prompt().then(data => writeHTML(team));
+prompt()
+    .then(team => 
+        writeHTML(team))
+    .then(HTML => console.log(HTML));
