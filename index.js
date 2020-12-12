def2f799f931eb51c1a260e5f59ac75bca2f2939
JGil <calterat@gmail.com>
2020-12-12 00:20:05 -0600

    some changes to the propmpt

--- index.js
+++ index.js
@@ -1,9 +1,83 @@
-const Prompt = require('./lib/Prompt');
-const prompt = new Prompt();
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
 
-let teamMember = [];
+const teamMember = [];
+let teamCount = 1;
 
-prompt.start(manager);
+// const newMember = (role) => {
+//     let member = {};
+//     if (!role) {
+//         member.role = 'Manager';
+//     } else {
+//         member.role = role;
+//     }
+//     console.log(member);
+//     promptForTeam(member);
+// }
 
-let basic = teamMember.push(prompt.basicInfo()
+const promptForTeam = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Input Name Here: ",
+            when: (teamCount) => {
+                if (teamCount ===1) return false;
+                else return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Input ID Here: '
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Input Email Here: '
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Input Office Numer Here: ',
+            when: ({ role }) => {
+                if (role === 'Manager') return true;
+                return false;
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Input Github Username Here: ',
+            when: ({ role }) => {
+                if (role === 'Engineer') return true;
+                return false;
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Input School Here: ',
+            when: ({ role }) => {
+                if (role  === 'Intern') return true;
+                return false;
+            }
+        },
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'Who would you like to add another to your team?',
+            choices: ['Engineer', 'Intern', 'Employee', 'NO']
+        }
+    ])
+    .then(newMember => {
+        teamMember.push(newMember);
+        if (newMember.choice === 'NO') return newMember;
+        else promptForTeam();
+    })
+}
+
+promptForTeam();
 
