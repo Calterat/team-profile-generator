a5b914733304da463c74a007ea94752750ff4d33
Calterat <Calterat@gmail.com>
2020-12-12 21:57:05 -0600

    initialized file write

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const { prompt } = require('./lib/Prompt');
 const { toClasses } = require('./lib/Classes');
 const writeHTML = require('./lib/HTML');
-const Employee = require('./lib/Employee');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const { writeFile } = require('./lib/File');
 
 prompt()
     .then(team => 
         toClasses(team))
     .then(team => 
         writeHTML(team))
-    .then(data => console.log(data))
+    .then(data => writeFile(data))
     .catch(err => console.log(err));
